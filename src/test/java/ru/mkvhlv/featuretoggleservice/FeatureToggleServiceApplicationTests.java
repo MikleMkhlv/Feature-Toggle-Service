e75e44c22ead752bdd38d5dfa9f72450c65e11c0
Mikhaylov Mikhail <mikhaylovms@yandex.ru>
2026-02-12 13:32:43 +0300

    first commit

---
+++ src/test/java/ru/mkvhlv/featuretoggleservice/FeatureToggleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mkvhlv.featuretoggleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureToggleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
